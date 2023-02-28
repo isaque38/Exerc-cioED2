d94b693c456e17bcbc73166e6b5a289602eabfb8
isaque38 <102200390+isaque38@users.noreply.github.com>
2023-02-28 20:14:22 -0300

    Add files via upload

---
+++ src/controller/converteBinario.java
@@ -0,0 +1,22 @@
+package controller;
+
+public class converteBinario {
+
+	public converteBinario() {
+
+		super();
+	}
+
+	public String binario(int num ) {
+		// condição de parada
+		if (num == 0) {
+			return "";
+
+		}
+		// função do termo anterior
+		int resto = num % 2;
+		num = num / 2;
+		return binario(num) + resto;
+	}
+
+}
